2f526dd3183c8469cc56c4d98f0c6c955a44bb69
LSS <lss@jaimal.com>
2023-10-27 12:04:37 +0200

    remove folder

---
+++ exo1.js
@@ -0,0 +1,11 @@
+var textContent = document.getElementById("text-content");
+var boldBtn = document.querySelector(".bold");
+var editText = () => {
+    if (cond) {
+        
+    }
+    textContent.style.fontWeight = "bold";
+
+}
+
+boldBtn.addEventListener("click", editText);
