3671b88d150cf42ab14336dbebd14799ed744cc1
LSS <lss@jaimal.com>
2023-10-27 12:14:01 +0200

    exo1 v1

--- exo1.js
+++ exo1.js
@@ -1,11 +1,42 @@
 var textContent = document.getElementById("text-content");
 var boldBtn = document.querySelector(".bold");
-var editText = () => {
-    if (cond) {
-        
-    }
-    textContent.style.fontWeight = "bold";
+var italicBtn = document.querySelector(".italic");
+var underlineBtn = document.querySelector(".underline");
+var clearBtn = document.querySelector(".clear");
+var textStyles = ["bold", "italic", "underline"];
 
+var boldText = () => {
+    if (textContent.style.fontWeight !== "bold") {
+        textContent.style.fontWeight = "bold";
+    }else{
+        textContent.style.fontWeight = "normal";
+    }
 }
 
-boldBtn.addEventListener("click", editText);
+var italicText = () => {
+    if (textContent.style.fontStyle !== "italic") {
+        textContent.style.fontStyle = "italic";
+    }else{
+        textContent.style.fontStyle = "normal";
+    }
+}
+var underlineText = () => {
+    if (textContent.style.textDecoration !== "underline") {
+        textContent.style.textDecoration = "underline";
+    }else{
+        textContent.style.textDecoration = "none";
+    }
+}
+
+
+
+var clearStyles = () => {
+    textContent.style.fontWeight = "normal";
+    textContent.style.fontStyle = "normal";
+    textContent.style.textDecoration = "none"; 
+}
+
+boldBtn.addEventListener("click", boldText);
+italicBtn.addEventListener("click", italicText);
+underlineBtn.addEventListener("click", underlineText);
+clearBtn.addEventListener("click", clearStyles);
